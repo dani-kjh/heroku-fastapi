c3b4e173cf29360221c5709bb9dc7b67d8bc38b2
DanielEscribano <daniescribanoperez@gmail.com>
2022-11-05 19:41:32 +0100

    testing inference

--- modules/inference.py
+++ modules/inference.py
@@ -1,12 +1,9 @@
 from transformers import T5Tokenizer, T5ForConditionalGeneration
 from codecarbon import track_emissions
 
-tokenizer = T5Tokenizer.from_pretrained("t5-small", model_max_length=256)
-model = T5ForConditionalGeneration.from_pretrained("t5-small")
+# tokenizer = T5Tokenizer.from_pretrained("t5-small")
+# model = T5ForConditionalGeneration.from_pretrained("t5-small")
 
-@track_emissions
+# @track_emissions
 def infer_t5(input):
-    input_ids = tokenizer(input, return_tensors="pt").input_ids
-    outputs = model.generate(input_ids)
-
-    return tokenizer.decode(outputs[0], skip_special_tokens=True)
+    return("balbalals")
